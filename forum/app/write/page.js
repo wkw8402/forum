6467797ec87ea6f8c440557b32491fd1a3b36f3b
Kyung-Wan Woo <wkw8402@berkeley.edu>
2024-01-03 22:31:39 -0800

    login complete

--- forum/app/write/page.js
+++ forum/app/write/page.js
@@ -1,11 +1,12 @@
 export default async function Write() {
-    return (
-      <div className="p-20">
-        <form action="/api/post/new" method="POST">
-          <input name="title" placeholder="title"/>
-          <input name="content" placeholder="content"/>
-          <button type="submit">submit</button>
-        </form>
-      </div>
-    )
+
+  return (
+    <div className="p-20">
+      <form action="/api/post/new" method="POST">
+        <input name="title" placeholder="title"/>
+        <input name="content" placeholder="content"/>
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  )
   } 
